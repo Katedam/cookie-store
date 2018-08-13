5b87f0fab304222a41add19b0d88e1ad5f21ecf0
Kate Dameron <katerj33@gmail.com>
2018-08-12 17:25:09 -0700

    changed all inner.html to inner.text

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var hours = ["10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"];
 
+
+
 var pioneerSquare = {
     location: "Pioneer Square",
     hoursOpen: hours,
@@ -12,15 +14,48 @@ var pioneerSquare = {
     cookiesPerHour: function() {
         return Math.floor(this.randomNumCustomers() * this.avgSalesPerCust);
     },
-    toHTML: function () {
+    makeTable: function () {
         var cookiesSold = 0;
-        var HTML = "<table><tr><th colspan='2'>" + this.location + "</th></tr><tr><th>Hour</th><th>Cookies Sold</th></tr>";
+        var list = document.getElementById("store-locations");
+        
+        var newTable = document.createElement("table");
+        newTable.setAttribute("class", "cookies");
+        list.appendChild(newTable);
+
+        var rowOne = document.createElement("tr");
+        newTable.appendChild(rowOne);
+        
+        var newTh = document.createElement("th");
+        newTh.setAttribute("colspan", "2");
+        rowOne.appendChild(newTh);
+        newTh.innerText = this.location
+
+        var rowTwo = document.createElement("tr");
+        newTable.appendChild(rowTwo);
+
+        var tdOne = document.createElement("td");
+        rowTwo.appendChild(tdOne);
+        tdOne.innerText = "Hour";
+
+        var tdTwo = document.createElement("td");
+        rowTwo.appendChild(tdTwo);
+        tdTwo.innerText = "Cookies Sold";
+
         for (var hourly = 0; hourly < hours.length; hourly++) {
-            HTML += "<tr><td>" + hours[hourly] + "</td><td>" + this.cookiesPerHour() + " cookies" + "</td></tr>";
-            cookiesSold += this.cookiesPerHour();
-        } 
-        HTML += "<tr><td colspan='2'>" + "Total: " + cookiesSold + "</td></tr>" + "</table>";
-        return HTML;
+            var hourlyCookies = this.cookiesPerHour();
+            var hourlyRows = document.createElement("tr");
+            newTable.appendChild(hourlyRows);
+            var time = document.createElement("td");
+            hourlyRows.appendChild(time);
+            time.innerText = hours[hourly];
+            var sales = document.createElement("td");
+            hourlyRows.appendChild(sales);
+            sales.innerText = hourlyCookies;
+            cookiesSold += hourlyCookies;
+        }
+        var totalRow = document.createElement("tr");
+        newTable.appendChild(totalRow);
+        totalRow.innerText = "Total: " + cookiesSold;
     }
 }
 
@@ -36,15 +71,48 @@ var portlandAirport = {
     cookiesPerHour: function() {
         return Math.floor(this.randomNumCustomers() * this.avgSalesPerCust);
     },
-    toHTML: function () {
+    makeTable: function () {
         var cookiesSold = 0;
-        var HTML = "<table><tr><th colspan='2'>" + this.location + "</th></tr><tr><th>Hour</th><th>Cookies Sold</th></tr>";
+        var list = document.getElementById("store-locations");
+        
+        var newTable = document.createElement("table");
+        newTable.setAttribute("class", "cookies");
+        list.appendChild(newTable);
+
+        var rowOne = document.createElement("tr");
+        newTable.appendChild(rowOne);
+        
+        var newTh = document.createElement("th");
+        newTh.setAttribute("colspan", "2");
+        rowOne.appendChild(newTh);
+        newTh.innerText = this.location
+
+        var rowTwo = document.createElement("tr");
+        newTable.appendChild(rowTwo);
+
+        var tdOne = document.createElement("td");
+        rowTwo.appendChild(tdOne);
+        tdOne.innerText = "Hour";
+
+        var tdTwo = document.createElement("td");
+        rowTwo.appendChild(tdTwo);
+        tdTwo.innerText = "Cookies Sold";
+
         for (var hourly = 0; hourly < hours.length; hourly++) {
-            HTML += "<tr><td>" + hours[hourly] + "</td><td>" + this.cookiesPerHour() + " cookies" + "</td></tr>";
-            cookiesSold += this.cookiesPerHour();
-        } 
-        HTML += "<tr><td colspan='2'>" + "Total: " + cookiesSold + "</td></tr>" + "</table>";
-        return HTML;
+            var hourlyCookies = this.cookiesPerHour();
+            var hourlyRows = document.createElement("tr");
+            newTable.appendChild(hourlyRows);
+            var time = document.createElement("td");
+            hourlyRows.appendChild(time);
+            time.innerText = hours[hourly];
+            var sales = document.createElement("td");
+            hourlyRows.appendChild(sales);
+            sales.innerText = hourlyCookies;
+            cookiesSold += hourlyCookies;
+        }
+        var totalRow = document.createElement("tr");
+        newTable.appendChild(totalRow);
+        totalRow.innerText = "Total: " + cookiesSold;
     }
 }
 
@@ -60,17 +128,51 @@ var washingtonSquare = {
     cookiesPerHour: function() {
         return Math.floor(this.randomNumCustomers() * this.avgSalesPerCust);
     },
-    toHTML: function () {
+    makeTable: function () {
         var cookiesSold = 0;
-        var HTML = "<table><tr><th colspan='2'>" + this.location + "</th></tr><tr><th>Hour</th><th>Cookies Sold</th></tr>";
+        var list = document.getElementById("store-locations");
+        
+        var newTable = document.createElement("table");
+        newTable.setAttribute("class", "cookies");
+        list.appendChild(newTable);
+
+        var rowOne = document.createElement("tr");
+        newTable.appendChild(rowOne);
+        
+        var newTh = document.createElement("th");
+        newTh.setAttribute("colspan", "2");
+        rowOne.appendChild(newTh);
+        newTh.innerText = this.location
+
+        var rowTwo = document.createElement("tr");
+        newTable.appendChild(rowTwo);
+
+        var tdOne = document.createElement("td");
+        rowTwo.appendChild(tdOne);
+        tdOne.innerText = "Hour";
+
+        var tdTwo = document.createElement("td");
+        rowTwo.appendChild(tdTwo);
+        tdTwo.innerText = "Cookies Sold";
+
         for (var hourly = 0; hourly < hours.length; hourly++) {
-            HTML += "<tr><td>" + hours[hourly] + "</td><td>" + this.cookiesPerHour() + " cookies" + "</td></tr>";
-            cookiesSold += this.cookiesPerHour();
-        } 
-        HTML += "<tr><td colspan='2'>" + "Total: " + cookiesSold + "</td></tr>" + "</table>";
-        return HTML;
+            var hourlyCookies = this.cookiesPerHour();
+            var hourlyRows = document.createElement("tr");
+            newTable.appendChild(hourlyRows);
+            var time = document.createElement("td");
+            hourlyRows.appendChild(time);
+            time.innerText = hours[hourly];
+            var sales = document.createElement("td");
+            hourlyRows.appendChild(sales);
+            sales.innerText = hourlyCookies;
+            cookiesSold += hourlyCookies;
+        }
+        var totalRow = document.createElement("tr");
+        newTable.appendChild(totalRow);
+        totalRow.innerText = "Total: " + cookiesSold;
     }
 }
+
 
 var sellwood = {
     location: "Sellwood",
@@ -84,17 +186,51 @@ var sellwood = {
     cookiesPerHour: function() {
         return Math.floor(this.randomNumCustomers() * this.avgSalesPerCust);
     },
-    toHTML: function () {
+    makeTable: function () {
         var cookiesSold = 0;
-        var HTML = "<table><tr><th colspan='2'>" + this.location + "</th></tr><tr><th>Hour</th><th>Cookies Sold</th></tr>";
+        var list = document.getElementById("store-locations");
+        
+        var newTable = document.createElement("table");
+        newTable.setAttribute("class", "cookies");
+        list.appendChild(newTable);
+
+        var rowOne = document.createElement("tr");
+        newTable.appendChild(rowOne);
+        
+        var newTh = document.createElement("th");
+        newTh.setAttribute("colspan", "2");
+        rowOne.appendChild(newTh);
+        newTh.innerText = this.location
+
+        var rowTwo = document.createElement("tr");
+        newTable.appendChild(rowTwo);
+
+        var tdOne = document.createElement("td");
+        rowTwo.appendChild(tdOne);
+        tdOne.innerText = "Hour";
+
+        var tdTwo = document.createElement("td");
+        rowTwo.appendChild(tdTwo);
+        tdTwo.innerText = "Cookies Sold";
+
         for (var hourly = 0; hourly < hours.length; hourly++) {
-            HTML += "<tr><td>" + hours[hourly] + "</td><td>" + this.cookiesPerHour() + " cookies" + "</td></tr>";
-            cookiesSold += this.cookiesPerHour();
-        } 
-        HTML += "<tr><td colspan='2'>" + "Total: " + cookiesSold + "</td></tr>" + "</table>";
-        return HTML;
+            var hourlyCookies = this.cookiesPerHour();
+            var hourlyRows = document.createElement("tr");
+            newTable.appendChild(hourlyRows);
+            var time = document.createElement("td");
+            hourlyRows.appendChild(time);
+            time.innerText = hours[hourly];
+            var sales = document.createElement("td");
+            hourlyRows.appendChild(sales);
+            sales.innerText = hourlyCookies;
+            cookiesSold += hourlyCookies;
+        }
+        var totalRow = document.createElement("tr");
+        newTable.appendChild(totalRow);
+        totalRow.innerText = "Total: " + cookiesSold;
     }
 }
+
 
 var pearlDistrict = {
     location: "Pearl District",
@@ -108,29 +244,81 @@ var pearlDistrict = {
     cookiesPerHour: function() {
         return Math.floor(this.randomNumCustomers() * this.avgSalesPerCust);
     },
-    toHTML: function () {
+    makeTable: function () {
         var cookiesSold = 0;
-        var HTML = "<table><tr><th colspan='2'>" + this.location + "</th></tr><tr><th>Hour</th><th>Cookies Sold</th></tr>";
+        var list = document.getElementById("store-locations");
+        
+        var newTable = document.createElement("table");
+        newTable.setAttribute("class", "cookies");
+        list.appendChild(newTable);
+
+        var rowOne = document.createElement("tr");
+        newTable.appendChild(rowOne);
+        
+        var newTh = document.createElement("th");
+        newTh.setAttribute("colspan", "2");
+        rowOne.appendChild(newTh);
+        newTh.innerText = this.location
+
+        var rowTwo = document.createElement("tr");
+        newTable.appendChild(rowTwo);
+
+        var tdOne = document.createElement("td");
+        rowTwo.appendChild(tdOne);
+        tdOne.innerText = "Hour";
+
+        var tdTwo = document.createElement("td");
+        rowTwo.appendChild(tdTwo);
+        tdTwo.innerText = "Cookies Sold";
+
         for (var hourly = 0; hourly < hours.length; hourly++) {
-            HTML += "<tr><td>" + hours[hourly] + "</td><td>" + this.cookiesPerHour() + " cookies" + "</td></tr>";
-            cookiesSold += this.cookiesPerHour();
-        } 
-        HTML += "<tr><td colspan='2'>" + "Total: " + cookiesSold + "</td></tr>" + "</table>";
-        return HTML;
+            var hourlyCookies = this.cookiesPerHour();
+            var hourlyRows = document.createElement("tr");
+            newTable.appendChild(hourlyRows);
+            var time = document.createElement("td");
+            hourlyRows.appendChild(time);
+            time.innerText = hours[hourly];
+            var sales = document.createElement("td");
+            hourlyRows.appendChild(sales);
+            sales.innerText = hourlyCookies;
+            cookiesSold += hourlyCookies;
+        }
+        var totalRow = document.createElement("tr");
+        newTable.appendChild(totalRow);
+        totalRow.innerText = "Total: " + cookiesSold;     
     }
 }
 
-var storeLocation = [
-    pioneerSquare,
-    portlandAirport,
-    washingtonSquare,
-    sellwood,
-    pearlDistrict,
-]
+    pioneerSquare.makeTable();
+    portlandAirport.makeTable();
+    washingtonSquare.makeTable();
+    sellwood.makeTable();
+    pearlDistrict.makeTable(); 
 
-var list = document.getElementById("store-locations");
-for (var index = 0; index < storeLocation.length; index++) {
-    list.innerHTML += storeLocation[index].toHTML();
-    console.log("make a list of store locations");
-}
+// toHTML: function () {
+    //     var cookiesSold = 0;
+    //     var HTML = "<table><tr><th colspan='2'>" + this.location + "</th></tr><tr><th>Hour</th><th>Cookies Sold</th></tr>";
+    //     
+    //         HTML += "<tr><td>" + hours[hourly] + "</td><td>" + hourlyCookies + " cookies" + "</td></tr>";
+    //         cookiesSold += hourlyCookies;
+    //     } 
+    //     HTML += "<tr><td colspan='2'>" + "Total: " + cookiesSold + "</td></tr>" + "</table>";
+    //     return HTML;
+    // }
+
+
+// var storeLocation = [
+//     pioneerSquare,
+//     portlandAirport,
+//     washingtonSquare,
+//     sellwood,
+//     pearlDistrict,
+// ]
+
+
+
+// for (var index = 0; index < storeLocation.length; index++) {
+//     list.innerHTML += storeLocation[index].toHTML();
+//     console.log("make a list of store locations");
+// }
 
